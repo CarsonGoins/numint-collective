9bffd256a6445d4d002aa63cfd0522b27cb5132b
CarsonGoins <71525076+CarsonGoins@users.noreply.github.com>
2021-12-13 22:52:46 -0500

    Update mpi_num_int.cpp

--- num_int/mpi_num_int.cpp
+++ num_int/mpi_num_int.cpp
@@ -17,7 +17,7 @@ extern "C" {
 }
 #endif
 
-float (*var) (float,int);
+float (*funccal) (float,int);
 
 int main (int argc, char* argv[])
 {
@@ -29,28 +29,28 @@ int main (int argc, char* argv[])
 
     int a = atoi(argv[2]);
     int n = atoi(argv[4]);
-    float m = (atoi(argv[3]) - a) / (float)n;
+    float multiplier = (atoi(argv[3]) - a) / (float)n;
     int intensity = atoi(argv[5]);
 
     int rank, size;
-    double global = 0.0;
+    double globalResult = 0.0;
 
     switch(atoi(argv[1]))
     {
         case 1:
-            var = &f1;
+            funccal = &f1;
             break;
         case 2:
-            var = &f2;
+            funccal = &f2;
             break;
         case 3:
-            var = &f3;
+            funccal = &f3;
             break;
         case 4:
-            var = &f4;
+            funccal = &f4;
             break;
         default:
-            std::cerr<<"nope";
+            std::cerr<<"Invalid function number provided\n";
             exit(-1);
     }
     std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
@@ -59,29 +59,29 @@ int main (int argc, char* argv[])
 
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    int temp1 = n / size;
-    int temp2 = rank * temp1;
-    int last = (rank + 1) * temp1;
+    int chunkSize = n / size;
+    int beg = rank * chunkSize;
+    int last = (rank + 1) * chunkSize;
     if (rank == size - 1)
-  
+        arrEnd = n;
 
-    double temp3 = 0.0;
-    for (int x = temp2; x < last; ++x)
+    double localres = 0.0;
+    for (int x = beg; x < last; ++x)
     {
-        temp3 += (double)var(a + (x + 0.5) * m, intensity) * m;
+        localres += (double)funccal(a + (x + 0.5) * multiplier, intensity) * multiplier;
     }
     if (rank != 0)
     {
-        MPI_Send(&temp3, 1, MPI_DOUBLE_PRECISION, 0, 100+rank, MPI_COMM_WORLD);
+        MPI_Send(&localres, 1, MPI_DOUBLE_PRECISION, 0, 100+rank, MPI_COMM_WORLD);
     }
     else
     {
-        global = temp3;
+        globalResult = localres;
         for (int x = 1; x < size; ++x)
         {
             MPI_Status status;
-            MPI_Recv(&temp3, 1, MPI_DOUBLE_PRECISION, x, 100+x, MPI_COMM_WORLD, &status);
-            global += temp3;
+            MPI_Recv(&localres, 1, MPI_DOUBLE_PRECISION, x, 100+x, MPI_COMM_WORLD, &status);
+            globalResult += localres;
         }
     }
 
@@ -91,7 +91,7 @@ int main (int argc, char* argv[])
 
     if(rank == 0)
     {
-        std::cout<<global<<std::endl;
+        std::cout<<globalResult<<std::endl;
         std::cerr<<elapsed_seconds.count()<<std::endl;
     }
 
